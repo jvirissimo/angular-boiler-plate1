b9a205e319dcbd2db81b570a8f4c2e484becde3d
jvirissimo <jvirissimo@gmail.com>
2020-04-08 14:14:42 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cool-app',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
