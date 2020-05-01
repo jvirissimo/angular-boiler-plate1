5d7a2eb0914c9d181c17bab0b0ca43042cf37a47
jvirissimo <jvirissimo@gmail.com>
2020-05-01 16:43:54 -0500

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
-import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
+import {LayoutModule} from './layout/layout.module';
+import { HttpService } from './http.service';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { WidgetsComponent } from './widgets/widgets.component';
 import { LoginComponent } from './login/login.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-import { LayoutComponent } from './layout/layout.component';
-
 
 @NgModule({
   imports:      [ AppRoutingModule, BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, ListComponent, WidgetsComponent, LoginComponent, LayoutComponent ],
+  declarations: [ AppComponent, ListComponent, WidgetsComponent, LoginComponent ],
   bootstrap:    [ AppComponent ],
   providers: [HttpService]
 })
